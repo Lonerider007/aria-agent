e93388a98290a855a335afaf44a51951639533de
Lonerider007 <samsungsumitv461@gmail.com>
2026-05-08 05:15:57 +0530

    v1.2.1: patch release

--- aria/ui/banner.py
+++ aria/ui/banner.py
@@ -3,7 +3,7 @@ from rich.text import Text
 from rich.align import Align
 from .console import console
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 LOGO = """[aria.primary]
     ◉  A R I A[/aria.primary]
